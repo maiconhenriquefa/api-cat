3ef77526f47bbe734d0c1a7103f8f07c9588030e
maiconhenriquefa <maiconhenriquefa@gmail.com>
2021-11-25 19:40:20 -0300

    fetching for cats

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 async function requisicao(url) {
   const response = await fetch(url);
   const json = await response.json();
-  return json;
+  return json[0].url;
 }
-requisicao('https://api.thecatapi.com/v1/images/search');
+
+const button = document.querySelector('button');
+
+async function handleClick() {
+  const img = document.querySelector('#photo');
+  img.innerHTML = `<img id='img-cat' src="${await requisicao(
+    'https://api.thecatapi.com/v1/images/search',
+  )}">`;
+}
+
+button.addEventListener('click', handleClick);
